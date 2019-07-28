3407c85a79d9a61f4ef86946e6c2c2ab0b3529f9
Sij <minecraftsij@foxmail.con>
2019-07-28 18:41:23 +0800

    all project

---
+++ 快速通讯/app/src/main/java/com/wvtsij/MainActivity.java
@@ -0,0 +1,30 @@
+package com.wvtsij;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+
+public class MainActivity extends AppCompatActivity {
+    
+	
+	private CardView cardView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+		
+
+
+        cardView = (CardView)findViewById(R.id.cardView);
+
+        cardView.setRadius(8);//设置图片圆角的半径大小
+
+        cardView.setCardElevation(8);//设置阴影部分大小
+
+        cardView.setContentPadding(5,5,5,5);//设置图片距离阴影大小
+		
+		
+    }
+    
+}
